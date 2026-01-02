5fe6cb819db9a16d58d2e24c6f3cdbd59fbe72d0
uvindukaralliyadda <134487180+uvindukaralliyadda@users.noreply.github.com>
2026-01-03 00:04:32 +0530

    fixed minor things

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
-import Whysolar from './Components/WhySolar/Whysolar'
-import Process from './Components/Process/Process'
-import Contact from './Components/Contact/Contact'
-import Leadership from './Components/Leadership/Leadership'
-import Footer from './Components/Footer/Footer'
-import Partners from './Components/Partners/Partners'
-import Testimonials from './Components/Testimonials/Testimonials'
+
+// Lazy load all components below About
+const Whysolar = React.lazy(() => import('./Components/WhySolar/Whysolar'))
+const Partners = React.lazy(() => import('./Components/Partners/Partners'))
+const Leadership = React.lazy(() => import('./Components/Leadership/Leadership'))
+const Testimonials = React.lazy(() => import('./Components/Testimonials/Testimonials'))
+const Process = React.lazy(() => import('./Components/Process/Process'))
+const Contact = React.lazy(() => import('./Components/Contact/Contact'))
+const Footer = React.lazy(() => import('./Components/Footer/Footer'))
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Whysolar/>
-      <Partners/>
-      <Leadership/>
-      <Testimonials/>
-      <Process/>
-      <Contact/>
-      <Footer/>
+      <Navbar />
+      <Hero />
+      <About />
+
+      <Suspense fallback={<div>Loading Why Solar...</div>}>
+        <Whysolar />
+      </Suspense>
+
+      <Suspense fallback={<div>Loading Partners...</div>}>
+        <Partners />
+      </Suspense>
+
+      <Suspense fallback={<div>Loading Leadership...</div>}>
+        <Leadership />
+      </Suspense>
+
+      <Suspense fallback={<div>Loading Testimonials...</div>}>
+        <Testimonials />
+      </Suspense>
+
+      <Suspense fallback={<div>Loading Process...</div>}>
+        <Process />
+      </Suspense>
+
+      <Suspense fallback={<div>Loading Contact...</div>}>
+        <Contact />
+      </Suspense>
+
+      <Suspense fallback={<div>Loading Footer...</div>}>
+        <Footer />
+      </Suspense>
     </div>
   )
 }
